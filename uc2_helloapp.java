1ad787e0454057c834acf0a207bbd1153a627d58
harshidha raja <harshidhav.raja@gmail.com>
2026-03-18 18:00:10 +0530

    uc2 done

---
+++ uc2_helloapp.java
@@ -0,0 +1,9 @@
+public class uc2_helloapp {
+    public static void main(String[] args) {
+        String name = "World"; 
+        if (args.length > 0) {
+            name = args[0];   
+        }
+        System.out.println("Hello, " + name + "!");
+    }
+}
